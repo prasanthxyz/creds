6ab3a4fe93851f225a6ee57acbeacb19cbb8ddd1
Prasanth P <prasanthxyz@gmail.com>
2022-05-15 19:23:08 +0530

    Add webService support for GET, PATCH, POST, DELETE methods generically
    - Test stub calls from DataView

--- frontend/src/_services/web.service.js
+++ frontend/src/_services/web.service.js
@@ -3,20 +3,38 @@ import { authHeader } from '../_helpers';
 import { userService } from './user.service';
 
 export const webService = {
+  req,
   handleResponse,
-  get,
 }
 
-function get(url) {
+export const HttpMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PATCH: 'PATCH',
+  DELETE: 'DELETE',
+};
+
+function req(method, url, data = {}) {
+  const methodsWithData = [HttpMethod.POST, HttpMethod.PATCH];
+
+  const headers = methodsWithData.includes(method)
+    ? Object.assign({}, authHeader(), { 'Content-Type': 'application/json' })
+    : authHeader();
   const requestOptions = {
-    method: 'GET',
-    headers: authHeader()
+    method: method,
+    headers: headers,
   };
+  if (methodsWithData.includes(method)) {
+    requestOptions['body'] = JSON.stringify(data);
+  }
 
   return fetch(`${config.apiUrl}${url}`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
+  if (response.status === 204) {
+    return { 'message': 'No content.' };
+  }
   return response.json().then(data => {
     if (!response.ok) {
       if (response.status === 401) {
--- frontend/src/views/DataView.vue
+++ frontend/src/views/DataView.vue
@@ -5,14 +5,19 @@
     </p>
     <p>
       <div v-for="item in data">
-        <p>{{item.content}}</p>
+        <p>{{item.id}}. {{item.content}}</p>
       </div>
+    </p>
+    <p>
+      <input type="button" @click="testPost()" value="Create Post"/><br/>
+      <input type="button" @click="testPatch()" value="Update Post"/><br/>
+      <input type="button" @click="testDelete()" value="Delete Post"/>
     </p>
   </div>
 </template>
 
 <script>
-import { webService } from '../_services';
+import { webService, HttpMethod } from '../_services';
 
 export default {
   data() {
@@ -22,9 +27,26 @@ export default {
   },
   created() {
     this.loading = true;
-    webService.get('/data/').then(data => {
+    webService.req(HttpMethod.GET, '/data/').then(data => {
       this.data = data;
     });
-  }
+  },
+  methods: {
+    testPost() {
+      webService.req(HttpMethod.POST, '/data/', {'content':'test_content_from_ajax'}).then(data => {
+        console.log(data);
+      });
+    },
+    testPatch() {
+      webService.req(HttpMethod.PATCH, '/data/19/', {'content':'test_content_from_ajax_patched'}).then(data => {
+        console.log(data);
+      });
+    },
+    testDelete() {
+      webService.req(HttpMethod.DELETE, '/data/13/').then(data => {
+        console.log(data);
+      });
+    },
+  },
 };
 </script>
